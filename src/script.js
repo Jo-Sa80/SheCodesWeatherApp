a955dde0bf1748a67c9698206d49c46c5c9b15e4
Jo-Sa80 <j.sandvoss80@gmail.com>
2024-11-26 15:17:25 +0100

    searchForm handeld

--- src/script.js
+++ src/script.js
@@ -1,10 +1,20 @@
-function getCity(response) {
-  let currentTemp = document.querySelector("#value");
-  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+//function getCity(response) {
+//let currentTemp = document.querySelector("#value");
+//currentTemp.innerHTML = Math.round(response.data.temperature.current);
+//}
+
+//let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
+//let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+//axios.get(apiUrl).then(getCity);
+
+function showSubmittedCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchInput");
+  let newCity = document.querySelector("#weather-city");
+
+  newCity.innerHTML = searchInput.value;
 }
+let searchElement = document.querySelector("#searchForm");
 
-let city = "Paris";
-let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-axios.get(apiUrl).then(getCity);
+searchElement.addEventListener("submit", showSubmittedCity);
