7afd5f33f56cba201e765ca9d551468348c14acf
Jo-Sa80 <j.sandvoss80@gmail.com>
2024-11-26 12:19:02 +0100

    worked on axios response

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
-function showCity() {
-  let newCity = document.querySelector("#weather-city");
-  newCity.innerHTML = `${city}`;
+function getCity(response) {
+  let currentTemp = document.querySelector("#value");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
 }
 
-showCity();
-
+let city = "Paris";
 let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={paris}&key={apiKey}";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showCity);
+axios.get(apiUrl).then(getCity);
