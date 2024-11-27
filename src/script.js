7f4a5481a5dd5e251a4ddb0b5ed89121246e4ab2
Jo-Sa80 <j.sandvoss80@gmail.com>
2024-11-27 13:14:24 +0100

    expanded Search

--- src/script.js
+++ src/script.js
@@ -1,20 +1,21 @@
-//function getCity(response) {
-//let currentTemp = document.querySelector("#value");
-//currentTemp.innerHTML = Math.round(response.data.temperature.current);
-//}
-
-//let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
-//let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-//axios.get(apiUrl).then(getCity);
+function showTemperature(response) {
+  let currentTemp = document.querySelector("#value");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+  let currentCity = document.querySelector("#weather-city");
+  currentCity.innerHTML = response.data.city;
+}
 
 function showSubmittedCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchInput");
-  let newCity = document.querySelector("#weather-city");
+  let city = searchInput.value;
 
-  newCity.innerHTML = searchInput.value;
+  console.log(city);
+  let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showTemperature);
 }
-let searchElement = document.querySelector("#searchForm");
 
+let searchElement = document.querySelector("#searchForm");
 searchElement.addEventListener("submit", showSubmittedCity);
